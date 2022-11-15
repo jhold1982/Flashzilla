174c11de7a657badc6c80f5a07ab8a7551b3d7b1
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-11-15 15:34:12 -0600

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Justin Hold on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
